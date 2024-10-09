53635da6c5aba30e202a68cbe526d4d80ae9e3da
Maximilian Alvim de Faria <maadf@kth.se>
2024-10-09 20:06:28 +0200

    Fix partstring to not use between/3

--- Labb1/partstring.pl
+++ Labb1/partstring.pl
@@ -1,11 +1,11 @@
 % partstring(List, F, L) - Generate the subset F of length L from List.
 partstring(List, F, L) :-
-    length(List, MaxL),
-    between(1, MaxL, L),    % Iterate over lengths from 1 to MaxL.
+    length(F, L),             % Ensure F has length L.
     substring(List, F, L).
 
 % substring(List, Sub, L) - Find a subset Sub of length L from List.
 substring(List, Sub, L) :-
-    append(_, Rest, List),  % Split List into two parts, ignoring the first part.
-    append(Sub, _, Rest),   % Split the Rest into Sub and the remaining part.
-    length(Sub, L).         % Ensure Sub has length L.
+    L > 0,                    % Ensure L is positive.
+    append(_, Rest, List),    % Split List into two parts, ignoring the first part.
+    append(Sub, _, Rest),     % Split the Rest into Sub and the remaining part.
+    length(Sub, L).           % Ensure Sub has length L.
