1dc80f648493a489d692a4e92b899e7e73ee146b
Maximilian Alvim de Faria <maadf@kth.se>
2024-10-09 20:37:58 +0200

    solution for partstring

--- Labb1/partstring.pl
+++ Labb1/partstring.pl
@@ -1,11 +1,10 @@
 % partstring(List, F, L) - Generate the subset F of length L from List.
 partstring(List, F, L) :-
-    length(F, L),             % Ensure F has length L.
-    substring(List, F, L).
+    substring(List, F),       % Get all consecutive substrings.
+    length(F, L).             % Ensure F has length L.
 
-% substring(List, Sub, L) - Find a subset Sub of length L from List.
-substring(List, Sub, L) :-
-    L > 0,                    % Ensure L is positive.
-    append(_, Rest, List),    % Split List into two parts, ignoring the first part.
-    append(Sub, _, Rest),     % Split the Rest into Sub and the remaining part.
-    length(Sub, L).           % Ensure Sub has length L.
+% substring(List, Sub) - Find all consecutive substrings of List.
+substring(List, Sub) :-
+    append(Sub, _, List).     % Split List into Sub and the remaining part.
+substring([_|Tail], Sub) :-
+    substring(Tail, Sub).     % Recursively find substrings in the tail.
